c4a46b146a3423289a3e47c3177c9eaaae2203e8
naganohara-yoshino <naganohara-yoshino@outlook.com>
2026-04-10 04:12:01 +0800

    feat: update to declarative macros

--- src/lib.rs
+++ src/lib.rs
@@ -1,80 +1,45 @@
-#![doc = include_str!("../README.md")]
-
-use proc_macro::TokenStream;
-use quote::quote;
-use syn::{Expr, parse_macro_input};
-
-/// Builds an `ordered_float::NotNan<f32>` from a const-evaluable expression.
-#[proc_macro]
-pub fn nn32(input: TokenStream) -> TokenStream {
-    expand_not_nan(input, FloatTy::F32)
-}
-
-/// Builds an `ordered_float::NotNan<f64>` from a const-evaluable expression.
-#[proc_macro]
-pub fn nn64(input: TokenStream) -> TokenStream {
-    expand_not_nan(input, FloatTy::F64)
-}
-
-/// Builds an `ordered_float::OrderedFloat<f32>` from an expression.
-#[proc_macro]
-pub fn of32(input: TokenStream) -> TokenStream {
-    expand_ordered_float(input, FloatTy::F32)
-}
-
-/// Builds an `ordered_float::OrderedFloat<f64>` from an expression.
-#[proc_macro]
-pub fn of64(input: TokenStream) -> TokenStream {
-    expand_ordered_float(input, FloatTy::F64)
-}
-
-#[derive(Clone, Copy)]
-enum FloatTy {
-    F32,
-    F64,
-}
-
-fn expand_not_nan(input: TokenStream, ty: FloatTy) -> TokenStream {
-    let expr = parse_macro_input!(input as Expr);
-
-    let ty_token = match ty {
-        FloatTy::F32 => quote!(f32),
-        FloatTy::F64 => quote!(f64),
-    };
-
-    let macro_name = match ty {
-        FloatTy::F32 => "nn32!",
-        FloatTy::F64 => "nn64!",
-    };
-
-    let expanded = quote! {{
-        const __ORDERED_FLOAT_MACROS_VALUE: #ty_token = #expr;
+#[macro_export]
+macro_rules! nn32 {
+    ($expr:expr) => {{
+        const __SHOULD_BE_CONST_IN_ORDERED_FLOAT_MACROS: f32 = $expr;
         const _: () = {
-            // This assertion runs at compile-time because it's in a const context.
-            assert!(!__ORDERED_FLOAT_MACROS_VALUE.is_nan(), concat!(#macro_name, ": expression evaluated to NAN"));
+            assert!(
+                !__SHOULD_BE_CONST_IN_ORDERED_FLOAT_MACROS.is_nan(),
+                "nn32!: expression evaluated to NAN"
+            );
         };
-        // SAFETY: The const assertion above ensures the value is not NaN.
-        // If the expression is not const-evaluable, the compiler will error
-        // at the __ORDERED_FLOAT_MACROS_VALUE definition.
         unsafe {
-            ::ordered_float::NotNan::<#ty_token>::new_unchecked(__ORDERED_FLOAT_MACROS_VALUE)
+            ::ordered_float::NotNan::<f32>::new_unchecked(__SHOULD_BE_CONST_IN_ORDERED_FLOAT_MACROS)
         }
     }};
-
-    expanded.into()
 }
 
-fn expand_ordered_float(input: TokenStream, ty: FloatTy) -> TokenStream {
-    let expr = parse_macro_input!(input as Expr);
+#[macro_export]
+macro_rules! nn64 {
+    ($expr:expr) => {{
+        const __SHOULD_BE_CONST_IN_ORDERED_FLOAT_MACROS: f64 = $expr;
+        const _: () = {
+            assert!(
+                !__SHOULD_BE_CONST_IN_ORDERED_FLOAT_MACROS.is_nan(),
+                "nn64!: expression evaluated to NAN"
+            );
+        };
+        unsafe {
+            ::ordered_float::NotNan::<f64>::new_unchecked(__SHOULD_BE_CONST_IN_ORDERED_FLOAT_MACROS)
+        }
+    }};
+}
 
-    let ty_token = match ty {
-        FloatTy::F32 => quote!(f32),
-        FloatTy::F64 => quote!(f64),
+#[macro_export]
+macro_rules! of32 {
+    ($expr:expr) => {
+        ::ordered_float::OrderedFloat::<f32>($expr)
     };
+}
 
-    let expanded = quote! {
-        ::ordered_float::OrderedFloat::<#ty_token>(#expr)
+#[macro_export]
+macro_rules! of64 {
+    ($expr:expr) => {
+        ::ordered_float::OrderedFloat::<f64>($expr)
     };
-
-    expanded.into()
 }
